29d84719339bb2233128cbcbe46ee2ba85a60e78
quyenha1225 <quyenha419@gmail.com>
2026-01-16 18:10:38 +0700

    Delete Chaper_1 directory

--- Chaper_1/src/main/java/Chaper_1/Bai_2/Bai_2.java
+++
@@ -1,8 +0,0 @@
-package Chaper_1;
-public class Bai_2 {
-    public static <E> void printArray(E[] inputArray){
-        for(E element : inputArray){
-        System.out.print(element+"");
-        }
-    }
-}
