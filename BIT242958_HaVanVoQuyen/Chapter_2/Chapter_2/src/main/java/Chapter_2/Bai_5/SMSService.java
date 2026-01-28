498770419b6a4b48beee7ef5ec9b89316cf31180
quyenha1225 <quyenha419@gmail.com>
2026-01-29 00:05:40 +0700

    Update SMSService.java

--- BIT242958_HaVanVoQuyen/Chapter_2/Chapter_2/src/main/java/Chapter_2/Bai_5/SMSService.java
+++ BIT242958_HaVanVoQuyen/Chapter_2/Chapter_2/src/main/java/Chapter_2/Bai_5/SMSService.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Chapter_2;
+package Chapter_2.Bai_5;
 
-/**
- *
- * @author Asus
- */
 public class SMSService implements MessageService {
+
     @Override
     public void sendMessage(String message) {
         System.out.println("Gửi SMS: " + message);
     }
 }
-
