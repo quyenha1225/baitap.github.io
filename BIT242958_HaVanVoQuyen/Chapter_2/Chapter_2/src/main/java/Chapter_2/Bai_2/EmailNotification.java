a38c1bb291f6b3ca75407cabcdf730a4d6527225
quyenha1225 <quyenha419@gmail.com>
2026-01-29 00:06:33 +0700

    Refactor EmailService to EmailNotification class

--- BIT242958_HaVanVoQuyen/Chapter_2/Chapter_2/src/main/java/Chapter_2/Bai_2/EmailNotification.java
+++ BIT242958_HaVanVoQuyen/Chapter_2/Chapter_2/src/main/java/Chapter_2/Bai_2/EmailNotification.java
@@ -2,14 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Chapter_2.Bai_5;
+package Chapter_2.Bai_2;
+
 /**
  *
  * @author Asus
  */
-public class EmailService implements MessageService {
-    @Override
-    public void sendMessage(String message) {
-        System.out.println("Gửi Email: " + message);
+public class EmailNotification implements Notification{
+    public void notifyUser(){
+        System.out.print("Gửi Push Notification");
     }
+ 
 }
